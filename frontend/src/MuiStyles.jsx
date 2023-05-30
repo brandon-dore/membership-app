358d226ca8466d614c17602639dd505173047e57
Amos V <amosvernon.dev@gmail.com>
2023-05-30 17:14:08 +0100

    finish create form

--- frontend/src/MuiStyles.jsx
+++ frontend/src/MuiStyles.jsx
@@ -5,12 +5,19 @@ export const modalBox = {
   transform: "translate(-50%, -50%)",
   width: "60rem",
   height: "40rem",
-  bgcolor: "white",
+  bgcolor: "lightgrey",
   border: "2px solid #000",
+  borderRadius: "4px",
   boxShadow: 24,
   p: 4,
 };
 
 export const textField = {
   width: "28rem",
+  color: "white",
+};
+
+export const sharpButton = {
+  borderTopRightRadius: "0",
+  borderTopLeftRadius: "0",
 };
--- frontend/src/components/CreateMember.jsx
+++ frontend/src/components/CreateMember.jsx
@@ -1,31 +1,39 @@
 import { Button, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./CreateMember.css";
-import { textField } from "../MuiStyles";
+import { sharpButton, textField } from "../MuiStyles";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Webcam from "react-webcam";
+import axios from "axios";
+
+const videoConstraints = {
+  width: 720,
+  height: 720,
+  facingMode: "user",
+};
 
 const CreateMember = () => {
-  const [x, setX] = useState(false);
-
   const [firstName, setFirstName] = useState("");
-
+  const [lastName, setLastName] = useState("");
+  const [dob, setDob] = useState("");
+  const [exp, setExp] = useState("");
   const [pic, setPic] = useState("");
 
-  useEffect(() => {
-    getX();
-  }, []);
-
-  const getX = () => {
-    fetch("")
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        setX(data);
-        console.log(data);
+  const handleSubmit = () => {
+    console.log(firstName, lastName, dob, exp);
+    const formdata = {
+      first_name: firstName,
+      last_name: lastName,
+      dob: dob,
+      expiry_date: exp,
+      photo: pic,
+    };
+    axios
+      .post("http://localhost:3000/users", formdata)
+      .then((res) => {
+        console.log(res);
       })
       .catch((e) => {
         console.log(e);
@@ -41,40 +49,66 @@ const CreateMember = () => {
         <form className="formContainer">
           <div className="inputFields">
             <TextField
-              onChange={() => setFirstName()}
+              onChange={(e) => setFirstName(e.target.value)}
               sx={textField}
               id="first_name"
               label="First Name"
               variant="outlined"
             />
             <TextField
-              onChange={() => setFirstName()}
+              onChange={(e) => setLastName(e.target.value)}
               sx={textField}
               id="last_name"
               label="Last Name"
               variant="outlined"
             />
             <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DatePicker label="D.O.B." />
+              <DatePicker
+                label="D.O.B."
+                onChange={(e) => setDob(e.format("yyyy-MM-DD"))}
+              />
             </LocalizationProvider>
             <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DatePicker label="Expiry Date" />
+              <DatePicker
+                label="Expiry Date"
+                onChange={(e) => setExp(e.format("yyyy-MM-DD"))}
+              />
             </LocalizationProvider>
           </div>
           <div className="photoContainer">
-            <Webcam>
-              {({ getScreenshot }) => {
+            {pic == "" ? (
+              <Webcam
+                audio={false}
+                screenshotFormat="image/jpeg"
+                videoConstraints={videoConstraints}
+              >
+                {({ getScreenshot }) => {
+                  return (
+                    <Button
+                      sx={sharpButton}
+                      onClick={() => setPic(getScreenshot())}
+                      variant="contained"
+                    >
+                      Take Picture
+                    </Button>
+                  );
+                }}
+              </Webcam>
+            ) : (
+              <>
+                <img src={pic} alt="Picture" />
                 <Button
-                  onClick={() => console.log(getScreenshot())}
+                  sx={sharpButton}
+                  onClick={() => setPic("")}
                   variant="contained"
                 >
-                  Take Picture
-                </Button>;
-              }}
-            </Webcam>
+                  Retake Picture
+                </Button>
+              </>
+            )}
           </div>
         </form>
-        <Button color="info" variant="outlined">
+        <Button onClick={handleSubmit} color="info" variant="contained">
           Create Member
         </Button>
       </div>
