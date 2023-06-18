5f4b79580b72e3b87bd5537514d547b5f265c451
Amos V <amosvernon.dev@gmail.com>
2023-06-18 23:39:06 +0100

    create working startup script

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-cd frontend
-npm run dev &
+# Below should be plcaed in the startup folder by doing: 
+# WIN+R
+# shell:startup
+# Create .sh file and place the following in there. 
+
+# Replace below with the proper file path
+cd ~/OneDrive/Documents/Projects/membership-app/frontend
+npm run preview &
 cd ../api
-node --watch index.js 
+node index.js  &
+start http://localhost:4173
+disown
